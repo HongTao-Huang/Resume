923d3ee8b9a6c583ffa418025e3896ada6b7b4ad
huanght <hht51770@hotmail.com>
2018-04-28 01:23:05 +0800

    animationNav

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 window.onload = function(){
     loading();
     listentopBar();
+    linstenMenu();
+    linstenNavA();
 }
 
 function loading() {
@@ -13,10 +15,36 @@ function loading() {
 function listentopBar() {
     window.onscroll = function () {
         if(window.scrollY > 0){
-            header.classList.add('active');
+            header.classList.add('sticky');
         }
         else {
-            header.classList.remove('active');
+            header.classList.remove('sticky');
+        }
+    }
+}
+
+function linstenMenu() {
+    let liTags = document.querySelectorAll('nav.menu > ul > li');
+    for(let i = 0; i < liTags.length; i++){
+        liTags[i].onmouseenter = function (e) {
+            e.currentTarget.classList.add('active');
+        }
+        liTags[i].onmouseleave = function (e) {
+            e.currentTarget.classList.remove('active');
+        }
+    }
+}
+
+function linstenNavA() {
+    let aTags = document.querySelectorAll('nav.menu > ul > li > a');
+    for(let i = 0; i < aTags.length; i++){
+        aTags[i].onclick = function (ev) {
+            ev.preventDefault();
+            let a = ev.currentTarget;
+            let href = a.getAttribute('href');
+            let element = document.querySelector(href);
+            let top = element.offsetTop;
+            window.scrollTo(0, top - 80);
         }
     }
 }
