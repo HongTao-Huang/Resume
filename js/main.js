26c5aa6494dd88446caa60c1a82cb81f17e0356f
huanght <hht51770@hotmail.com>
2018-05-02 23:05:20 +0800

    导航栏缓动

--- js/main.js
+++ js/main.js
@@ -38,13 +38,42 @@ function linstenMenu() {
 function linstenNavA() {
     let aTags = document.querySelectorAll('nav.menu > ul > li > a');
     for(let i = 0; i < aTags.length; i++){
-        aTags[i].onclick = function (ev) {
-            ev.preventDefault();
-            let a = ev.currentTarget;
+        aTags[i].onclick = function (e) {
+            e.preventDefault();
+            let a = e.currentTarget;
             let href = a.getAttribute('href');
             let element = document.querySelector(href);
-            let top = element.offsetTop;
-            window.scrollTo(0, top - 80);
+            let targetTop = element.offsetTop - 80;
+            // let n = 20;
+            // let t =  500 / n;
+            let currentTop = window.scrollY;
+            let s = (Math.abs(targetTop - currentTop))/ 100;
+            // id = setInterval( ()=> {           //生硬的动画
+            //     if(n-- < 0){
+            //         window.clearInterval(id);
+            //         return;
+            //     }
+            //     i = i + 1;
+            //     window.scrollTo(0, currentTop + s * i);
+            // },t);
+            /* 缓动*/
+            function animate(time) {
+                requestAnimationFrame(animate);
+                TWEEN.update(time);
+            }
+            requestAnimationFrame(animate);
+            let t = s * 300;
+            if(t > 900){
+              t = 900;
+            }
+            var coords = { y: currentTop }; // Start at (0, 0)
+            var tween = new TWEEN.Tween(coords)
+                .to({ y : targetTop }, t)
+                .easing(TWEEN.Easing.Quadratic.InOut)
+                .onUpdate(function() {
+                    window.scrollTo(0 , coords.y);
+                })
+                .start();
         }
     }
 }
